95b5f7aa5eb177d483cb14f57f2dbdf6c72c072b
Bibek <kumar.bee014@gmail.com>
2021-01-15 01:33:52 +0545

    RestController feature implemented in CategpryController

--- src/main/java/com/mycodestuffs/springmvcrest/controllers/v1/CategoryController.java
+++ src/main/java/com/mycodestuffs/springmvcrest/controllers/v1/CategoryController.java
@@ -6,11 +6,9 @@ import com.mycodestuffs.springmvcrest.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/categories/")
 public class CategoryController {
 
@@ -21,13 +19,20 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories(){
-        return new ResponseEntity<CategoryListDTO>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
+    // old code-- above code is due to restController
+
+//    public ResponseEntity<CategoryListDTO> getAllCategories(){
+//        return new ResponseEntity<CategoryListDTO>(
+//                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+//    }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
-        return new ResponseEntity<CategoryDTO>(categoryService.getCategoryByName(name),HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
